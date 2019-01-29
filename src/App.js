718f31923adf0780ffe8fa64d1fdcd56c736815a
Katelyn Salvatori <katelyn.salvatori@gmail.com>
2019-01-28 21:42:39 -0500

    Started Pokemon type matchup app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {value: '', error: null, items: [], submitted: ''};
+
+    this.apiUrl = "http://pokeapi.co/api/v2/";
+    this.pokemonUrl = this.apiUrl + "pokemon/";
+    this.typeUrl = this.apiUrl + "type/";
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({submitted: this.state.value});
+    alert('A name was submitted: ' + this.state.value);
+    this.findTypeMatchups();
+  }
+
+  findTypeMatchups() {
+    var pokemon = this.state.value.replace(/'/g, "").replace(/\./g, "").replace(/ /g, "-").toLowerCase();
+    console.log(this.pokemonUrl + pokemon);
+    fetch(this.pokemonUrl + pokemon, {mode: 'cors'})
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+          this.setState({error});
+        }
+      );
+  }
+
   render() {
+    const value = this.state.submitted;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form autoComplete="off" onSubmit={this.handleSubmit}>
+          <label>
+            Pokémon name: <br />
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <h1>{value}</h1>
       </div>
     );
   }
